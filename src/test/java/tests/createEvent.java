6da5359cb4d964b6a0182101828274dfde2531f8
juxel <yspostbox@yahoo.com>
2019-12-02 22:04:36 +0100

    some small changes

--- src/test/java/tests/createEvent.java
+++ src/test/java/tests/createEvent.java
@@ -4,9 +4,9 @@ public class createEvent {
 
     /* create a class
     * add the class to staging area--> git add .
-    * commit it to local repository--> commit -m"i added event class
-    * "
-    * push it to the GitHub-->
+    * commit it to local repository--> commit -m"i added event class"
+    * push it to the GitHub-->git push
+    *
     * */
 
 }
