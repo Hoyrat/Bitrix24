2e3ce3bd8d7f0ddeac31047cf96887403f985a03
juxel <yspostbox@yahoo.com>
2019-12-02 21:59:30 +0100

    new event class added

---
+++ src/test/java/tests/createEvent.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class createEvent {
+
+    /* create a class
+    * add the class to staging area--> git add .
+    * commit it to local repository--> commit -m"i added event class
+    * "
+    * push it to the GitHub-->
+    * */
+
+}
