d7e5e96be915232b43a95282567dadf92cfab7bc
juxel <yspostbox@yahoo.com>
2019-12-09 19:15:40 +0100

    added method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,10 +1,13 @@
 package tests;
 
 public class task {
-public static void main({String[] args){
+public static void main(String[] args){
 
   System.out.println("Hello World, from github");
-  
+
 }
 
+public void hi(){
+  System.out.println("Hi from InteliJ");
+    }
 }
