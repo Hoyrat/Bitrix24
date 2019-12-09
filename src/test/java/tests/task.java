8ad3de4be5389b070a9f4f2ad578091db0e38ea1
Hoyrat <53577734+Hoyrat@users.noreply.github.com>
2019-12-09 18:45:08 +0100

    new changes

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -4,6 +4,7 @@ public class task {
 public static void main({String[] args){
 
   System.out.println("Hello World, from github");
+  //this code is written on github and has been seen on intelij
   
 }
 
