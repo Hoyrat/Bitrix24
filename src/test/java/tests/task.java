e58056352df748851180d4d41d73796cc65f45da
Hoyrat <53577734+Hoyrat@users.noreply.github.com>
2019-12-09 19:18:24 +0100

    added return method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -5,7 +5,10 @@ public static void main({String[] args){
 
   System.out.println("Hello World, from github");
   //this code is written on github and has been seen on intelij
-  
-}
+    }
 
+   //bu satir githubtan yazildi
+     public String hello(){
+     return"Hello from Github"
+     }
 }
