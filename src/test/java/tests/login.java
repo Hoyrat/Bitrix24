dd2ef2a1eb409af9983e61fe4cf981033cdb2939
juxel <yspostbox@yahoo.com>
2019-12-16 18:55:39 +0100

    update

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -2,27 +2,13 @@ package tests;
 
 public class login {
 {
-
-
-  
-  
-  
-  this is wriite on GitHUb
-    
-    16.12.2019
-    
-    3.changes
-
+        // this is written on GitHub
+        //  16.12.2019
+       //  3.changes
 
 
     //this line written on IDE
-
     // this is written on IDE too
 }
-
-
-
-
-
 
 }
